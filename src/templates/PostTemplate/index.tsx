b36a4fb323300c071f8465ef91eca71a51c7f320
Luiz Henrique <7henrique18@gmail.com>
2022-03-20 19:09:00 -0300

    :gear: Fix broken build

--- src/templates/PostTemplate/index.tsx
+++ src/templates/PostTemplate/index.tsx
@@ -1,30 +1,11 @@
-import { Header } from "components/Header"
 import { GetPostsQuery } from "graphql/generated/graphql"
-import { useRef } from "react"
-
-import * as S from "./styles"
 
 type PostsTemplateProps = {
   posts: GetPostsQuery["posts"]
 }
 
 export const PostTemplate = (props: PostsTemplateProps) => {
-  const { posts } = props
-  const videoRef = useRef<HTMLVideoElement>(null)
+  console.log(props)
 
-  return (
-    <>
-      <S.VideoOverlay />
-      <S.VideoBanner muted autoPlay loop ref={videoRef}>
-        <source src="/videos/galaxy.mp4" type="video/mp4" />
-      </S.VideoBanner>
-
-      <S.HeroContainer>
-        <Header />
-        <HeroSection />
-      </S.HeroContainer>
-
-      <PostsList posts={posts} />
-    </>
-  )
+  return <></>
 }
