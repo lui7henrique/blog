b5382a29b9eff40586ef9ec6bdab5d78eecdc099
Luiz Henrique <7henrique18@gmail.com>
2022-05-27 23:07:26 -0300

    :bug: Change issue-term to title

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,23 +1,25 @@
+import { useRouter } from "next/router"
 import { useEffect, useRef } from "react"
 
 import * as S from "./styles"
 
 export const Comments = () => {
   const ref = useRef<HTMLDivElement>(null)
+  const { pathname } = useRouter()
 
   useEffect(() => {
     const scriptEl = document.createElement("script")
     scriptEl.setAttribute("src", "https://utteranc.es/client.js")
     scriptEl.setAttribute("async", "true")
     scriptEl.setAttribute("repo", "lui7henrique/blog")
-    scriptEl.setAttribute("issue-term", "pathname")
+    scriptEl.setAttribute("issue-term", "title")
     scriptEl.setAttribute("theme", "github-dark")
     scriptEl.setAttribute("crossorigin", "anonymous")
 
     if (ref) {
       ref.current?.appendChild(scriptEl)
     }
-  }, [])
+  }, [pathname])
 
   return <S.Container ref={ref} />
 }
