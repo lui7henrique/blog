3420463292b489a61cfa8090568708e5a99eb902
Luiz Henrique <7henrique18@gmail.com>
2023-01-21 02:24:22 -0300

    fix: Uterrance theme

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,11 +1,16 @@
+import { useTheme } from "next-themes"
 import { useRouter } from "next/router"
 import { useEffect, useRef } from "react"
 
 import * as S from "./styles"
 
 export const Comments = () => {
+  const { resolvedTheme } = useTheme()
+
   const ref = useRef<HTMLDivElement>(null)
   const { pathname } = useRouter()
+
+  const theme = resolvedTheme === "dark" ? "github-dark" : "github-light"
 
   useEffect(() => {
     const scriptEl = document.createElement("script")
@@ -13,13 +18,29 @@ export const Comments = () => {
     scriptEl.setAttribute("async", "true")
     scriptEl.setAttribute("repo", "lui7henrique/blog")
     scriptEl.setAttribute("issue-term", "title")
-    scriptEl.setAttribute("theme", "github-dark")
+    scriptEl.setAttribute("theme", theme)
     scriptEl.setAttribute("crossorigin", "anonymous")
 
     if (ref) {
       ref.current?.appendChild(scriptEl)
     }
   }, [pathname])
+
+  useEffect(() => {
+    if (document.querySelector(".utterances-frame")) {
+      const iframe =
+        document.querySelector<HTMLIFrameElement>(".utterances-frame")
+
+      if (!iframe) {
+        return
+      }
+
+      iframe?.contentWindow?.postMessage(
+        { type: "set-theme", theme: theme },
+        "https://utteranc.es"
+      )
+    }
+  }, [theme])
 
   return <S.Container ref={ref} />
 }
